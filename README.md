020e814178a741e189171673917891d6f0640fb4
Carlos Bruno <carlao.linux@gmail.com>
2020-04-19 13:59:23 -0300

    Project completed

---
+++ README.md
@@ -0,0 +1,25 @@
+# Introduction to Flutter Development with Dart
+## **Mi Card** -  A Single Screen Personal Business Card App
+
+**Used in the project**
+- MaterialApp
+- Scaffold
+- SafeArea
+- Column
+- CircleAvatar
+- Text
+- SizedBox - Divider (for line)
+- Card - LisTile (leading: Icon | title: Text)  
+
+**Google fonts** 
+- Pacifico - Regular
+- Source Sans Pro - Regular
+
+<div style="text-align: right">
+
+~~Carlos Bruno~~
+
+## Instructor: Angela Yu
+# FlutterBrew 🛰️
+
+</div>
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/bruno_01.jpg'),
+              ),
+              Text(
+                'Carlos Bruno',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'FLUTTER BEGINNER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 170.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.code,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    'https://github.com/suricarlos',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    'carlao.linux@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
